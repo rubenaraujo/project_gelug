6ebc37c181a01db27e29ef60b62f7ecd2f6d2157
Ruben Araujo (CTW) <Ruben.Araujo@ctw.bmwgroup.com>
2023-02-24 14:22:54 +0000

    Version 2.0.0
    Signed-off-by: Ruben Araujo (CTW) <Ruben.Araujo@ctw.bmwgroup.com>

---
+++ assets/js/gelug.js
@@ -0,0 +1,52 @@
+function setButtonEvents() {
+  const buttons = document.querySelectorAll("#buttons > button")
+  const images = document.querySelectorAll("img")
+  const sliders = document.querySelectorAll(`input[type="range"]`)
+  const buttonMute = document.querySelector("#btn-mute")
+
+  buttons.forEach(button => {
+    button.addEventListener("click", event => {
+      button.classList.toggle("active")
+    })
+  })
+
+  images.forEach(image => {
+    image.addEventListener("click", event => {
+      const key = image.parentElement.dataset.key
+
+      image.classList.toggle("active")
+      const slider = document.querySelector(`div[data-key="${key}"] > input[type="range"]`)
+      slider.classList.toggle("slider-active")
+
+      const audio = document.querySelector(`audio[data-key="${key}"]`)
+      if (audio)
+        audio.paused ? audio.play() : audio.pause()
+    })
+  })
+
+  sliders.forEach(slider => {
+    slider.addEventListener("input", event => {
+      const key = slider.parentElement.attributes["data-key"].value
+
+      const audio = document.querySelector(`audio[data-key="${key}"]`)
+      if (audio)
+        audio.volume = slider.value / (slider.max - slider.min)
+    })
+  })
+
+  buttonMute.addEventListener("click", event => {
+    buttonMute.classList.toggle("fa-volume-up")
+    buttonMute.classList.toggle("fa-volume-off")
+
+    const audios = document.querySelectorAll("audio")
+    audios.forEach(audio => {
+      audio.muted = !audio.muted
+    })
+  })
+}
+
+function run() {
+  setButtonEvents()
+}
+
+run()
